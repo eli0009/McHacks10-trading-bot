cc866af15e83982317ea0449c3c46536b219f6ff
Enlai Li <enlai.li@mail.mcgill.ca>
2023-01-28 15:09:38 -0500

    testing

--- webScraper.py
+++ webScraper.py
@@ -2,3 +2,6 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+df_yahoo = yf.download("BTC", start="2020-09-15", end="2020-11-15", progress=False)
+print(df_yahoo.head())

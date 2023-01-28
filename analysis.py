3aacbe01ecdf7baa39f8bbc3e96e6cb7e649fadd
Enlai Li <enlai.li@mail.mcgill.ca>
2023-01-28 17:00:54 -0500

    sample analysis

--- analysis.py
+++ analysis.py
@@ -1,8 +1,158 @@
 from negatives import words
 
-article = ""
+article = """
+
+    Buying & Selling Crypto Exchanges 
+
+The Collapse of FTX: What Went Wrong With the Crypto Exchange?
+By Nathan Reiff
+Updated January 04, 2023
+Fact checked by Vikki Velasquez
+
+Cryptocurrency exchange FTX and its founder and former CEO, Sam Bankman-Fried, are intricately entwined. The swift and damaging collapse of FTX in late 2022 will have repercussions on the international crypto community for years to come.
+
+Learn more about what went wrong with FTX.
+Key Takeaways
+
+    FTX collapsed in early November 2022 following a report by CoinDesk highlighting potential leverage and solvency concerns involving FTX-affiliated trading firm Alameda Research.
+    FTX’s collapse shook the volatile crypto market, which lost billions at the time, falling below a $1 trillion valuation.
+    FTX in November 2022 faced a liquidity crisis and searched for bailout funds; rival exchange Binance considered buying portions of the company but quickly backed out.
+    By Nov. 11, 2022, FTX’s CEO stepped down and the company filed for bankruptcy.
+    In the hours following, FTX experienced a possible hack in which hundreds of millions worth of tokens were stolen.
+    FTX founder and ex-CEO Sam Bankman-Fried was arrested in the Bahamas and extradited to the U.S. in late December. He pleaded innocent to all criminal charges Jan. 3.
+
+What Happened to FTX
+
+FTX’s collapse took place over a 10-day period in November 2022. The catalyst was a Nov. 2 scoop by crypto news site CoinDesk that revealed that Alameda Research, the quant trading firm also run by Bankman-Fried, held a position valued at $5 billion in FTT, the native token of FTX.
+
+The report disclosed that Alameda’s investment foundation was also in FTT, the token that its sister company had invented, not a fiat currency or other cryptocurrency.1 That prompted concern across the cryptocurrency industry regarding Bankman-Fried’s companies’ undisclosed leverage and solvency.2
+FTX Collapse's Sequence of Events
+
+The following is a recap of the events that led to FTX's failure, bankruptcy filing, and its former CEO's jailing and extradition to the U.S. to face a series of criminal and civil charges.
+FTX Collapse Timeline - 2022
+
+    Nov. 6: Rival exchange Binance sells all FTT tokens.
+    Nov. 7: FTX announces liquidity crisis; seeks bailout from venture capitalists, then Binance.
+    Nov. 8: Binance says it will buy FTX's non-U.S. business.
+    Nov. 9: Binance walks away from FTX acquisition after conducting due diligence.
+    Nov. 10: Bahamas freezes assets of FTX's subsidiary there; Bankman-Fried admits non-U.S. businesses' liquidity crisis, says affiliate Alameda Research to wind down.
+    Nov. 11: Bankman-Fried steps down as FTX CEO; is replaced by a court-appointed CEO with restructuring experience. FTX files for Chapter 11 bankruptcy protection.
+    Nov. 12: FTX reports an alleged hack, suspected to be up to $477 million; moves its digital assets to cold storage for security reasons.
+    Nov. 18: The Bahamas takes control of FTX assets held there.
+    Dec. 12: Bankman-Fried is arrested by Bahamian authorities. He's later extradited to the U.S.
+    Dec. 22: Bankman-Fried is released on a $250 million bond, the largest in history, by a federal judge. 
+
+Binance Says It Will Sell All Its FTT Tokens
+
+Binance, the world’s biggest crypto exchange, announced on Nov. 6 that it would sell its entire position in FTT tokens—roughly 23 million FTT tokens valued at about $529 million. Binance CEO Changpeng “CZ” Zhao said the decision to liquidate the exchange’s FTT position was based on risk management, following the collapse of the Terra (LUNA) crypto token earlier in 2022.34
+FTX Liquidity Crisis and Binance Deal
+
+By the next day, FTX was experiencing a liquidity crisis. Bankman-Fried attempted to reassure FTX investors that its assets were stable, but customers demanded withdrawals worth $6 billion in the days immediately following the CoinDesk report. Bankman-Fried searched for additional money from venture capitalists before turning to rival Binance. The value of FTT fell by more than 80% in two days.56
+
+On Nov. 8, Binance announced it had reached a nonbinding agreement to buy the non-U.S. business of FTX for an undisclosed sum—effectively the world’s largest cryptocurrency exchange bailing out its close rival.7
+Binance Cancels Deal to Bail Out FTX
+
+The promise of a rescue was short-lived, as Binance backed out of the deal a day later. On Nov. 9, the exchange said that it would cancel the FTX deal after corporate due diligence raised concerns about the mishandling of customer funds, among other issues.8
+FTX Assets Frozen and Other Implications
+
+On Nov. 10, the Bahamas securities regulator froze the assets of FTX Digital Markets, FTX’s Bahamian subsidiary, following news that Bankman-Fried was seeking up to $8 billion in capital to bail out the exchange.9 On the same day, the California Department of Financial Protection and Innovation announced that it had initiated an investigation into FTX.10
+
+Bankman-Fried apologized the same day for the liquidity crisis and admitted on Twitter that FTX’s non-U.S. exchange had insufficient funds to meet customer demands. He said that “poor internal labeling” caused FTX to miscalculate leverage and liquidity. In the same Twitter thread, he said Alameda would wind down trading.11
+Bankman-Fried Steps Down as CEO; FTX Files for Bankruptcy
+
+Bankman-Fried stepped down on Nov. 11 as CEO of FTX, replaced by court-appointed FTX CEO John Ray, who led energy trading firm Enron through bankruptcy proceedings years before.12
+
+FTX filed for Chapter 11 bankruptcy protection the same day, revealing that roughly 130 other affiliated companies were also part of the proceedings. The bankruptcy filings indicated that FTX had assets in the range of $10 billion to $50 billion and liabilities in the range of $10 billion to $50 billion.13
+
+Investors and customers have lost billions, and not all of it will be recovered, the new FTX CEO told a U.S. House of Representatives committee hearing on the FTX debacle held on Dec. 13.
+‘Unauthorized Transactions’ on FTX
+
+Within hours of filing for bankruptcy, FTX said it was the victim of “unauthorized transactions” and that it would move its digital assets to cold storage for security purposes.14 Outside analysts said they suspect that about $477 million was stolen from FTX in the alleged hack.15
+Lawsuit Against FTX, Celebrity Promoters Filed
+
+On Nov. 16, a class-action lawsuit was filed in a Florida federal court, alleging that Bankman-Fried created a fraudulent cryptocurrency scheme designed to take advantage of unsophisticated investors from across the U.S. Others named in the lawsuit include celebrities and professional athletes Steph Curry, Shaquille O’Neal, Shohei Ohtani, Naomi Osaka, Larry David, and Kevin O’Leary, who allegedly helped Bankman-Fried facilitate the plan.16
+The Bahamas Takes Control of FTX Digital Assets
+
+The Securities Commission of the Bahamas (SCB) took control of cryptocurrency assets held by bankrupt exchange FTX on Nov. 18. The SCB said it instructed Bankman-Fried to move crypto assets to the regulator’s wallet to protect creditors.
+Arrest, Charges Against Bankman-Fried
+
+Bahamian authorities arrested Bankman-Fried on Dec. 12, 2022, and jailed him in connection with multiple fraud charges involving FTX, including those stemming from an indictment from the U.S. Attorney of the Southern District of New York. U.S. Attorney Damian Williams said when announcing the charges against the former CEO that it was one of the largest financial crimes in American history. New FTX CEO Ray told the U.S. House committee on Dec. 13 that FTX practiced "no bookkeeping." He added: "It was old-fashioned embezzlement."17
+
+Bankman-Fried was extradited to the U.S., then indicted by the U.S. District Court in Manhattan on eight counts, including securities fraud and money laundering. Following a court hearing on Dec. 22, a federal judge decided to release Bankman-Fried from custody after his attorneys and federal prosecutors agreed to a $250 million bond, the largest in history. The 30-year-old former crypto executive will live with his Stanford law professor parents in Palo Alto, California, be confined to the Northern California area, and wear an electronic monitoring bracelet.18
+
+On Jan. 3, 2023, Bankman-Fried pleaded not guilty to all criminal charges in a federal court in New York. He is set to face trial on eight charges on Oct. 2.19
+Future of FTX and Consequences of Collapse
+
+The future of FTX as a cryptocurrency exchange is in serious jeopardy. As of mid-November 2022, withdrawals were disabled and a notice on the FTX website says the company “strongly advise[s] against depositing.”20
+
+The broader consequences of the FTX fiasco for the cryptocurrency industry will take time to unfold. As the largest collapse in the short history of cryptocurrencies, FTX may further deter investors, who already are cautious because of concerns about stability and security. Customers on the FTX platform may not recover their assets, potentially triggering legal action. The U.S. Securities and Exchange Commission (SEC) and other regulators may see the collapse of FTX as justification for tightening regulatory scrutiny of cryptocurrencies, and Congress may be more inclined to step in and create new laws governing digital tokens and exchanges.21
+
+The stunning collapse of the third-largest crypto exchange by volume will send shock waves through the crypto universe for some time. To date, crypto lender BlockFi paused client withdrawals on Nov. 11, 2022, and rumors indicate it may have a dicey future. Crypto.com saw withdrawals increase from Nov. 12 to 13, 2022. Genesis Global Capital halted customer withdrawals from its crypto lending unit.
+How Did FTX Fail?
+
+FTX filed for bankruptcy on Nov. 11, 2022, after a surge of customer withdrawals earlier in the month. Then-CEO Bankman-Fried admitted that the company didn't have sufficient assets in reserve to meet customer demand.
+Did FTX Get Hacked?
+
+Within hours of filing for bankruptcy, FTX alleged it was hacked. The exchange noted “unauthorized transactions” that may have stolen close to $500 million in assets, and which were spotted by Elliptic, a crypto compliance service. The hacker continued to drain wallets for several days using what analysts called “on-chain spoofing.” The hacker reportedly then invested those funds in Ether (ETH).22
+What Happens Next to Ex-FTX CEO Bankman-Fried?
+
+Bankman-Fried is set to face trial on eight criminal charges on Oct. 2. After a court hearing on Dec. 22, 2023, a federal judge decided to release Bankman-Fried from custody after his attorneys and federal prosecutors agreed to a $250 million bond, the largest ever. He will live with his law professor parents in Palo Alto, California, be confined to the Northern California area, and wear an electronic monitoring bracelet.18 On Jan. 3, 2023, Bankman-Fried pleaded not guilty to all criminal charges in a federal court in New York.
+The Bottom Line
+
+The fortunes of FTX and its founder and former CEO, Bankman-Fried, are linked. In November 2022, cryptocurrency exchange FTX collapsed over a period of 10 days. Following a report suggesting potential leverage and solvency concerns, the exchange faced a liquidity crisis and tried to negotiate a bailout by rival Binance that quickly fell through. Its CEO was later arrested, extradited to the U.S., released on a $250 bond, and he now faces trial in October.
+
+The implications for the future of FTX and the collapse's impact on the broader cryptocurrency industry are ongoing and difficult to assess.
+Article Sources
+Related Articles
+Cryptocurrencies on laptop
+
+Cryptocurrency News
+Crypto News Outlet CoinDesk Explores Sale as DCG Crisis Worsens
+Sam Bankman-Fried at a congressional hearing
+
+CEOs
+Who Is Sam Bankman-Fried?
+FTX founder Sam Bankman-Fried leaves Manhattan federal court
+
+Cryptocurrency News
+Bankruptcy Reveals $1 Billion FTX-Tied Stake in Bitcoin Miner
+Tampa Bay Buccaneers Quarterback Tom Brady at the 2021 Super Bowl
+
+Cryptocurrency News
+FTX Bankruptcy Hearing Shows Tom Brady, Ex-Wife's Stake Loss Could Be $70M
+Sam Bankman-Fried, former CEO of FTX cryptocurrency exchange
+
+Cryptocurrency News
+Sam Bankman-Fried to Be Released on $250 Million Bail
+Cryptocurrency coins over a tablet displaying a trading app
+
+Cryptocurrency News
+Crypto Winter Gets Icier Just Days Into New Year
+Related Terms
+FTX: An Overview of the Exchange and Its Collapse
+FTX, once the third-largest centralized cryptocurrency exchange, is now in U.S. bankruptcy. Its founder faces criminal and civil charges and goes to trial in October.
+more
+Who Is Changpeng Zhao?
+Changpeng Zhao is the founder and CEO of the world’s latest cryptocurrency exchange, Binance.
+more
+SEC vs. Ripple
+Ripple Labs, creator of the XRP cryptocurrency token, has been caught up in an SEC lawsuit for years. The outcome could have repercussions for cryptocurrency regulation.
+more
+FTX US Derivatives (Formerly LedgerX)
+FTX US Derivatives (Formerly LedgerX) was an exchange and clearinghouse specializing in cryptocurrency derivatives.
+more
+Proof of Reserves: Could It Have Avoided the FTX Meltdown?
+Proof of reserves (PoR) is a transparent auditing practice for cryptocurrency companies that provides an unbiased report of the companies’ reserve assets.
+more
+What Is XRP?
+What is XRP? This is the native cryptocurrency of XRP Ledger, an open-source, public blockchain.
+more
+"""
 
 negative_score = 0
 for word in words:
     if word in article:
         negative_score += 1
+
+print(f"Negative score: {negative_score}")
+print(f"Total words: {len(article.split())}")

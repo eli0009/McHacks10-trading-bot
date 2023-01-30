0ccb490258c8dae82e1d1fa2e6fd5c0caa99b216
Eric Huang <eric.huang3143@gmail.com>
2023-01-30 17:04:53 -0500

    Pre-topic ml addition

--- mainML.py
+++ mainML.py
@@ -2,33 +2,51 @@ from model import sentiment
 from MarketNews import get_articles, get_page_text, Article
 from typing import List
 
-def analyse_news(articleObjs:List[Article]):
+def analyse_news(articleObjs:List[Article], a = ""):
     recommendationScore = 0
-    for article in articleObjs:
-        print(article)
-        title, textLines = get_page_text(article.url)
-        textLines.append(title)
+
+    if a :
+        print('Processing all titles...')
+        textLines = [get_page_text(article.url)[0] for article in articleObjs]
         recommendationScore = sentiment(textLines)
-        print("Done")
+        print("Done!")
+    else:
+        for article in articleObjs:
+            print(f"Analysing article:{article}...")
+            title, textLines = get_page_text(article.url)
+            if a !="":
+                textLines = []
+            textLines.append(title)
+            recommendationScore = sentiment(textLines)
+            print("Done")
 
     return recommendationScore
 
-def get_stock_prediction(ticker:str, threshold = 0.66):
+def get_stock_prediction(ticker:str, threshold = 0.66, a= "" ):
     articleObjs = get_articles(symbol = ticker)
-    recommendationScore = analyse_news(articleObjs)
+    recommendationScore = analyse_news(articleObjs, a)
 
-    buy, hold, sell = threshold, 0, -threshold
+    buy, hold, sell = threshold, 0, (-1*threshold)
 
     if recommendationScore> buy:
-        return 1
+        return "Buy\n"
     elif recommendationScore < sell:
-        return -1
+        return "Sell\n"
     else:
-        return hold
+        return "Hold\n"
 
 
 
 if __name__ == "__main__":
-    stock = input("Pleae enter a large cap US Equity Symbol (ex: AAPL): ").upper()
-    decision = get_stock_prediction(stock)
-    print(decision)
+    while input != 'q':
+        print("Hit q to quit")
+        stock = ""
+        attempts = 0
+        while not stock and attempts < 2:
+            stock = input("Pleae enter a large cap US Equity Symbol (ex: AAPL): ").upper()
+            if stock: attempts = 0
+            else: attempts +=1
+
+        a = input("Fast version (hit any key for yes/just hit enter for no)?")
+        decision = get_stock_prediction(stock,a = a)
+        print(decision)
